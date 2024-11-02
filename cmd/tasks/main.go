e891113f516807fc50dcf168385df0f831631f19
Meraiku <meraikuu@gmail.com>
2024-11-02 23:56:12 +0300

    Done task 7

--- cmd/tasks/main.go
+++ cmd/tasks/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/meraiku/db_tasks/internal/repo/postgres_sqlc"
+)
 
 func main() {
-	fmt.Println("Init")
+	godotenv.Load()
+	ctx := context.TODO()
+	pool, err := pgxpool.New(ctx, os.Getenv("POSTGRES_DSN"))
+	if err != nil {
+		panic(err)
+	}
+	defer pool.Close()
+
+	queries := postgres_sqlc.New(pool)
+
+	_, err = queries.AddDepartment(ctx, "IT")
+	if err != nil {
+		panic(err)
+	}
+	queries.AddDepartment(ctx, "HR")
+	queries.AddDepartment(ctx, "Sales")
+
+	_, err = queries.AddEmployee(ctx, postgres_sqlc.AddEmployeeParams{
+		FirstName:    "John",
+		LastName:     "Doe",
+		DepartmentID: 1,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	queries.AddEmployee(ctx, postgres_sqlc.AddEmployeeParams{
+		FirstName:    "Jane",
+		LastName:     "Doe",
+		DepartmentID: 1,
+	})
+
+	queries.AddEmployee(ctx, postgres_sqlc.AddEmployeeParams{
+		FirstName:    "Joe",
+		LastName:     "Doe",
+		DepartmentID: 2,
+	})
+
+	queries.AddEmployee(ctx, postgres_sqlc.AddEmployeeParams{
+		FirstName:    "Jill",
+		LastName:     "Doe",
+		DepartmentID: 3,
+	})
+
+	if err := queries.AddProject(ctx, "Project 1"); err != nil {
+		panic(err)
+	}
+	queries.AddProject(ctx, "Project 2")
+	queries.AddProject(ctx, "Project 3")
+
 }
--- sql/queries/tasks.sql
+++ sql/queries/tasks.sql
@@ -1,5 +1,5 @@
 -- name: TaskThreeGetITEmployees :many
-SELECT (first_name, last_name) AS full_name
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 INNER JOIN departments ON employees.department_id = departments.id
 WHERE departments.title = 'IT';
@@ -12,3 +12,12 @@ WHERE id = 1;
 -- name: TaskFiveDeleteProject :exec
 DELETE FROM projects
 WHERE id = 2;
+
+-- name: TaskSevenAggFunc :many
+SELECT 
+	departments.title AS department,
+  COUNT(employees.id) AS total_employees
+FROM employees
+INNER JOIN departments ON employees.department_id = departments.id
+GROUP BY departments.title
+ORDER BY total DESC;
